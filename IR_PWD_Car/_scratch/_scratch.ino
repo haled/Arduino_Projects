2c452705d1a7e56b90d85f04a1d54334a01ae8e4
Darren Hale <darrenhale@darrenhale.clearent.lan>
2016-01-18 08:14:48 -0600

    Added some libraries and starter sketches so that I don't lose anything.

---
+++ IR_PWD_Car/_scratch/_scratch.ino
@@ -0,0 +1,38 @@
+#include <IRremote.h>
+#include <IRremoteInt.h>
+
+IRrecv irrecv(12);
+decode_results results;
+
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(11, OUTPUT);
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+}
+
+void loop()
+{
+  if (irrecv.decode(&results))
+  {
+    digitalWrite(13, HIGH);
+    delay(1000);
+    digitalWrite(13, LOW);
+    //delay(1000);
+
+    if(results.value == 16724175)  // 1
+    {
+      digitalWrite(11, HIGH);
+    }
+    else
+    {
+      digitalWrite(11, LOW);
+    }
+    
+    //Serial.println(results.value, HEX);
+    Serial.println(results.value);
+    irrecv.resume();
+  }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Arduino_Projects
+This repository is a dumping ground of my Arduino sketches so that I don't lose anything.
